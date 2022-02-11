d141338c2c0badc0563b18d16ef64dc83c00f318
thadsa <94719073+thadsa@users.noreply.github.com>
2022-02-10 19:13:45 -0500

    C#

--- Math Operation Assignment/MathAssignment.cs/Properties/AssemblyInfo.cs
+++ Math Operation Assignment/MathAssignment.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Math Operation Assignment Submission")]
+[assembly: AssemblyTitle("Math Operation Assignment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Math Operation Assignment Submission")]
+[assembly: AssemblyProduct("Math Operation Assignment")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
