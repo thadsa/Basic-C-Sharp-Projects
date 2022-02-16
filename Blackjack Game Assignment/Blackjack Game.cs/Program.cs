7bc5de2cd32ecace73e9cbb7d3f76e182ff4a2d0
thadsa <94719073+thadsa@users.noreply.github.com>
2022-02-16 13:52:32 -0500

    blackjack

---
+++ Blackjack Game Assignment/Blackjack Game.cs/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Game_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {}
+            class ExceptionEntity
+        {
+            public int Id { get; set; }
+            public string ExceptionType { get; set; }
+            public string ExceptionMessage { get; set; }
+            public DateTime TimeStamp { get; set; }
+        }
+    }
+    }
